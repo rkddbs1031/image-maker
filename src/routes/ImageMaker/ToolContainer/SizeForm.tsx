8209f5e1551fbadcd3d77fb1f0917c48e1ba2490
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2023-12-20 18:26:13 +0900

    Fix: SizeForm component 모듈화

---
+++ src/routes/ImageMaker/ToolContainer/SizeForm.tsx
@@ -0,0 +1,53 @@
+import { ChangeEvent, useState } from 'react'
+import styles from './toolContainer.module.scss'
+import { Size } from 'types/tool'
+
+const InitSize = { width: 300, height: 500 }
+
+const SizeForm = () => {
+  const [size, setSize] = useState<Size>(InitSize)
+
+  const handleChangeWidth = (e: ChangeEvent<HTMLInputElement>) => {
+    const width = Number(e.currentTarget.value) || 1
+    setSize((prev) => ({ ...prev, width }))
+  }
+
+  const handleChangeHeight = (e: ChangeEvent<HTMLInputElement>) => {
+    const height = Number(e.currentTarget.value) || 1
+    setSize((prev) => ({ ...prev, height }))
+  }
+
+  return (
+    <div className={styles.size_container}>
+      <dl>
+        <dt>사이즈</dt>
+        <dd>
+          <div className={styles.input_container}>
+            <span className={styles.option_title}>width</span>
+            <input
+              type='text'
+              name='width'
+              value={size.width}
+              min={1}
+              onChange={handleChangeWidth}
+              className={styles.input}
+            />
+          </div>
+          <div className={styles.input_container}>
+            <span className={styles.option_title}>height</span>
+            <input
+              type='text'
+              name='height'
+              value={size.height}
+              min={1}
+              onChange={handleChangeHeight}
+              className={styles.input}
+            />
+          </div>
+        </dd>
+      </dl>
+    </div>
+  )
+}
+
+export default SizeForm
--- src/routes/ImageMaker/ToolContainer/index.tsx
+++ src/routes/ImageMaker/ToolContainer/index.tsx
@@ -1,54 +1,10 @@
-import { ChangeEvent, useState } from 'react'
+import SizeForm from './\bSizeForm'
 import styles from './toolContainer.module.scss'
-import { Size } from 'types/tool'
-
-const InitSize = { width: 300, height: 500 }
 
 const ToolContainer = () => {
-  const [size, setSize] = useState<Size>(InitSize)
-
-  const handleChangeWidth = (e: ChangeEvent<HTMLInputElement>) => {
-    const width = Number(e.currentTarget.value) || 1
-    setSize((prev) => ({ ...prev, width }))
-  }
-
-  const handleChangeHeight = (e: ChangeEvent<HTMLInputElement>) => {
-    const height = Number(e.currentTarget.value) || 1
-    setSize((prev) => ({ ...prev, height }))
-  }
-
   return (
     <div className={styles.tool_container}>
-      <div className={styles.size_container}>
-        <dl>
-          <dt>사이즈</dt>
-          <dd>
-            <div className={styles.input_container}>
-              <span className={styles.option_title}>width</span>
-              <input
-                type='text'
-                name='width'
-                value={size.width}
-                min={1}
-                onChange={handleChangeWidth}
-                className={styles.input}
-              />
-            </div>
-            <div className={styles.input_container}>
-              <span className={styles.option_title}>height</span>
-              <input
-                type='text'
-                name='height'
-                value={size.height}
-                min={1}
-                onChange={handleChangeHeight}
-                className={styles.input}
-              />
-            </div>
-          </dd>
-        </dl>
-      </div>
-
+      <SizeForm />
       <button type='button'>Download</button>
     </div>
   )
