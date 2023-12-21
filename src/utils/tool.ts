5d772e5dbb636a4963551cc617ff160e8b1b2ab7
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2023-12-21 17:23:46 +0900

    Add: alignList 추가

---
+++ src/utils/tool.ts
@@ -0,0 +1,7 @@
+import { AlignList, AlignType } from 'types/tool'
+
+export const alignList: AlignList[] = [
+  { key: AlignType.CENTER, label: '중앙정렬' },
+  { key: AlignType.LEFT, label: '좌측정렬' },
+  { key: AlignType.RIGHT, label: '우측정렬' },
+]
