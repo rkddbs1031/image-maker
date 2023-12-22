14ddd71a45d9bcb72f3a841309f2284d770554e0
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2023-12-22 12:05:19 +0900

    Fix: styleState에 fontSize, fontWeight, fontFamily 추가

--- src/states/tool.ts
+++ src/states/tool.ts
@@ -1,12 +1,14 @@
 import { atom } from 'recoil'
-import { AlignType, StyleState } from 'types/tool'
+import { AlignType, FontFamilyType, StyleState } from 'types/tool'
 
 export const styleState = atom<StyleState>({
   key: '#styleState',
   default: {
     width: 300,
     height: 500,
-    alignType: AlignType.CENTER,
-    fontSize: 10,
+    alignType: AlignType.Center,
+    fontSize: 20,
+    fontWeight: 400,
+    fontFamily: FontFamilyType.NanumGothicCoding,
   },
 })
