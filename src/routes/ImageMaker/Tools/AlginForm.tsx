979ef059707c33f6b609e1e10fd5e0b7a557ce48
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2023-12-22 11:09:06 +0900

    Fix: AlignForm component에 DropDown component 적용

--- src/routes/ImageMaker/Tools/AlginForm.tsx
+++ src/routes/ImageMaker/Tools/AlginForm.tsx
@@ -1,43 +1,20 @@
-import { useState } from 'react'
+import { useRecoilState } from 'recoil'
 
 import { AlignType } from 'types/tool'
 import { alignList } from 'utils/tool'
-
-import { cx } from 'styles'
-import styles from './toolContainer.module.scss'
-import { useRecoilState } from 'recoil'
 import { styleState } from 'states/tool'
+
+import styles from './toolContainer.module.scss'
+import DropDown from 'components/DropDown'
 
 const AlignForm = () => {
   const [style, setStyle] = useRecoilState(styleState)
-  const [isDropped, setIsDropped] = useState(false)
 
-  const align = alignList.find(({ key }) => key === style.alignType)?.label
-
-  const handleDropDown = () => setIsDropped((prev) => !prev)
-
-  const handleClickAlign = (key: AlignType) => {
-    setStyle((prev) => ({ ...prev, alignType: key }))
-    setIsDropped((prev) => !prev)
-  }
+  const handleChangeAlginType = (alignType: AlignType) => setStyle((prev) => ({ ...prev, alignType }))
 
   return (
     <div className={styles.align_container}>
-      <button type='button' className={styles.selected_algin} onClick={handleDropDown}>
-        {align}
-      </button>
-
-      <div className={cx(styles.dropdown_list_container, { [styles.isActive]: isDropped })}>
-        <ul>
-          {alignList.map(({ key, label }) => (
-            <li key={key}>
-              <button type='button' onClick={() => handleClickAlign(key)}>
-                {label}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <DropDown defaultValue={style.alignType} list={alignList} onClickDropDownList={handleChangeAlginType} />
     </div>
   )
 }
