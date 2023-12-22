13624640558fc22c22dcb550a9356d6968ad3b58
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2023-12-22 16:21:21 +0900

    Fix: image_bg영역 추가

--- src/routes/ImageMaker/Tools/BackgroundEditor.tsx
+++ src/routes/ImageMaker/Tools/BackgroundEditor.tsx
@@ -1,4 +1,7 @@
+import { ChangeEvent } from 'react'
 import { useRecoilState } from 'recoil'
+
+import { FaImage } from 'react-icons/fa6'
 
 import { styleState } from 'states/tool'
 
@@ -10,15 +13,33 @@ import styles from './toolContainer.module.scss'
 const BackgroundEditor = () => {
   const [style, setStyle] = useRecoilState(styleState)
 
-  const handleChangeBGColor = (backgroundColor: string) => setStyle((prev) => ({ ...prev, backgroundColor }))
+  const handleChangeBGColor = (backgroundColor: string) =>
+    setStyle((prev) => ({ ...prev, backgroundColor, backgroundImage: null }))
 
+  const handleChangeBGImage = (e: ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target
+    if (!files) return
+
+    const backgroundImage = URL.createObjectURL(files[0])
+    setStyle((prev) => ({ ...prev, backgroundImage }))
+  }
   return (
     <div className={styles.background_editor_container}>
       <div className={cx([styles.bg_editor, styles.color_bg])}>
         <ColorPicker defaultValue={style.backgroundColor} onChangeColor={handleChangeBGColor} />
       </div>
 
-      <div className={cx([styles.bg_editor, styles.image_bg])}>image bg</div>
+      <div className={cx([styles.bg_editor, styles.image_bg])}>
+        <label htmlFor='background_image'>
+          <FaImage />
+        </label>
+        <input
+          type='file'
+          id='background_image'
+          accept='image/png, image/jpeg, image/gif'
+          onChange={handleChangeBGImage}
+        />
+      </div>
     </div>
   )
 }
