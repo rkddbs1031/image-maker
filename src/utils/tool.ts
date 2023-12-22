0377a6262568a83227de9bc2154e7cc86f9d0564
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2023-12-22 12:05:48 +0900

    Fix: fontSizeList, fontWeightList, fontFamilyList 추가

--- src/utils/tool.ts
+++ src/utils/tool.ts
@@ -1,12 +1,12 @@
-import { AlignList, AlignType, FontSizeList } from 'types/tool'
+import { AlignList, AlignType, FontFamilyList, FontFamilyType, FontListType } from 'types/tool'
 
 export const alignList: AlignList[] = [
-  { key: AlignType.CENTER, label: '중앙정렬' },
-  { key: AlignType.LEFT, label: '좌측정렬' },
-  { key: AlignType.RIGHT, label: '우측정렬' },
+  { key: AlignType.Center, label: '중앙정렬' },
+  { key: AlignType.Left, label: '좌측정렬' },
+  { key: AlignType.Right, label: '우측정렬' },
 ]
 
-export const fontSizeList: FontSizeList[] = [
+export const fontSizeList: FontListType[] = [
   { key: 10, label: '10px' },
   { key: 20, label: '20px' },
   { key: 30, label: '30px' },
@@ -17,4 +17,20 @@ export const fontSizeList: FontSizeList[] = [
   { key: 80, label: '80px' },
   { key: 90, label: '90px' },
   { key: 100, label: '100px' },
+]
+
+export const fontWeightList: FontListType[] = [
+  { key: 100, label: '100' },
+  { key: 200, label: '200' },
+  { key: 300, label: '300' },
+  { key: 400, label: '400' },
+  { key: 500, label: '500' },
+  { key: 600, label: '600' },
+  { key: 700, label: '700' },
+  { key: 800, label: '800' },
+]
+
+export const fontFamilyList: FontFamilyList[] = [
+  { key: FontFamilyType.NanumGothicCoding, label: 'Nanum Gothic Coding' },
+  { key: FontFamilyType.Arial, label: 'Arial' },
 ]
