59b512f0e5c71f3cd05e5682707ae4a2eddf21d0
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2023-12-26 11:47:30 +0900

    Fix: ImageViewer 수정

--- src/routes/ImageMaker/ImageViewer/index.tsx
+++ src/routes/ImageMaker/ImageViewer/index.tsx
@@ -1,12 +1,11 @@
-import { ChangeEvent } from 'react'
-import { useRecoilState } from 'recoil'
+import { useRecoilValue } from 'recoil'
 
 import { styleState } from 'states/tool'
 
 import styles from './imageViewer.module.scss'
 
 const ImageViewer = () => {
-  const [style, setStyle] = useRecoilState(styleState)
+  const style = useRecoilValue(styleState)
 
   const layoutStyle = {
     width: style.width,
@@ -23,19 +22,12 @@ const ImageViewer = () => {
     color: style.color,
   }
 
-  const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) =>
-    setStyle((prev) => ({ ...prev, title: e.currentTarget.value }))
-
   return (
     <div className={styles.image_viewer_container}>
-      <div className={styles.canvas} style={layoutStyle}>
-        <input
-          type='text'
-          value={style.title}
-          className={styles.title}
-          style={inputStyle}
-          onChange={handleChangeTitle}
-        />
+      <div className={styles.canvas} style={layoutStyle} id='canvas'>
+        <p style={inputStyle} className={styles.title}>
+          {style.title}
+        </p>
       </div>
     </div>
   )
