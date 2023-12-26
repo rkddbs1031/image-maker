4afe00b9d78c148f924400e8077a54d6e6042b95
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2023-12-26 11:47:50 +0900

    Fix: 이미지 다운로드 받는 기능 구현 완료

--- src/routes/ImageMaker/index.tsx
+++ src/routes/ImageMaker/index.tsx
@@ -1,9 +1,35 @@
+import { useRecoilValue } from 'recoil'
+
+import html2canvas from 'html2canvas'
+
+import { styleState } from 'states/tool'
+
 import Tools from './Tools'
 import ImageViewer from './ImageViewer'
 
 import styles from './imageMaker.module.scss'
 
 const ImageMaker = () => {
+  const { width, height, title } = useRecoilValue(styleState)
+
+  const handleCapture = () => {
+    const canvasTarget = document.getElementById('canvas')
+
+    if (!canvasTarget) return
+
+    html2canvas(canvasTarget, { width, height, scale: 1 }).then((canvas) => {
+      const link = document.createElement('a')
+
+      document.body.appendChild(link)
+
+      link.href = canvas.toDataURL('image/png')
+      link.download = `${title.split(' ').join('_')}.png`
+      link.click()
+
+      document.body.removeChild(link)
+    })
+  }
+
   return (
     <section id={styles.content}>
       <h1 className='blind'>이미지 메이커</h1>
@@ -13,7 +39,9 @@ const ImageMaker = () => {
         <Tools />
 
         <div className={styles.download_container}>
-          <button type='button'>DownLoad</button>
+          <button type='button' onClick={handleCapture}>
+            DownLoad
+          </button>
         </div>
       </div>
     </section>
