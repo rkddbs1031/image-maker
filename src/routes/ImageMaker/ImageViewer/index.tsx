dae4ebe60bd250ec4ef1e5a7fab74fd407de23c0
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2023-12-26 10:10:39 +0900

    Fix: ImageViewer component에 style 적용 및 수정

--- src/routes/ImageMaker/ImageViewer/index.tsx
+++ src/routes/ImageMaker/ImageViewer/index.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent } from 'react'
 import { useRecoilState } from 'recoil'
 
 import { styleState } from 'states/tool'
@@ -7,28 +8,33 @@ import styles from './imageViewer.module.scss'
 const ImageViewer = () => {
   const [style, setStyle] = useRecoilState(styleState)
 
+  const layoutStyle = {
+    width: style.width,
+    height: style.height,
+    ...(style.backgroundColor && { backgroundColor: style.backgroundColor }),
+    ...(style.backgroundImage && { backgroundImage: `url(${style.backgroundImage})` }),
+  }
+
+  const inputStyle = {
+    textAlign: style.alignType,
+    fontSize: style.fontSize,
+    fontWeight: style.fontWeight,
+    fontFamily: style.fontFamily,
+    color: style.color,
+  }
+
+  const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) =>
+    setStyle((prev) => ({ ...prev, title: e.currentTarget.value }))
+
   return (
     <div className={styles.image_viewer_container}>
-      <div
-        className={styles.canvas}
-        style={{
-          width: style.width,
-          height: style.height,
-          backgroundSize: 'cover',
-          background: style.backgroundImage ? `url(${style.backgroundImage}) no-repeat center` : style.backgroundColor,
-        }}
-      >
+      <div className={styles.canvas} style={layoutStyle}>
         <input
           type='text'
           value={style.title}
           className={styles.title}
-          style={{
-            textAlign: style.alignType,
-            fontSize: style.fontSize,
-            fontWeight: style.fontWeight,
-            fontFamily: style.fontFamily,
-            color: style.color,
-          }}
+          style={inputStyle}
+          onChange={handleChangeTitle}
         />
       </div>
     </div>
