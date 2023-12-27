cac936c970bd02d40d1d504d3cb96e914d23217c
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2023-12-27 14:37:17 +0900

    Fix: dimLayer, blur tool 추가

--- src/routes/ImageMaker/Tools/BackgroundEditor.tsx
+++ src/routes/ImageMaker/Tools/BackgroundEditor.tsx
@@ -1,9 +1,11 @@
-import { ChangeEvent } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { useRecoilState } from 'recoil'
 
 import { FaImage } from 'react-icons/fa6'
+import { BsLayersHalf } from 'react-icons/bs'
 
 import { styleState } from 'states/tool'
+import { INIT_BLUR } from 'utils/tool'
 
 import ColorPicker from 'components/ColorPicker'
 
@@ -12,6 +14,7 @@ import styles from './toolContainer.module.scss'
 
 const BackgroundEditor = () => {
   const [style, setStyle] = useRecoilState(styleState)
+  const [hasDimLayer, setHasDimLayer] = useState(false)
 
   const handleChangeBGColor = (backgroundColor: string) =>
     setStyle((prev) => ({ ...prev, backgroundColor, backgroundImage: null }))
@@ -23,6 +26,15 @@ const BackgroundEditor = () => {
     const backgroundImage = URL.createObjectURL(files[0])
     setStyle((prev) => ({ ...prev, backgroundImage }))
   }
+
+  const handleCheckedDimLayerValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setHasDimLayer(e.currentTarget.checked)
+    setStyle((prev) => ({ ...prev, blur: e.currentTarget.checked ? INIT_BLUR : null }))
+  }
+
+  const handleChangeOpacity = (e: ChangeEvent<HTMLInputElement>) =>
+    setStyle((prev) => ({ ...prev, blur: Number(e.currentTarget.value) }))
+
   return (
     <div className={styles.background_editor_container}>
       <div className={cx([styles.bg_editor, styles.color_bg])}>
@@ -39,6 +51,26 @@ const BackgroundEditor = () => {
           accept='image/png, image/jpeg, image/gif'
           onChange={handleChangeBGImage}
         />
+      </div>
+
+      <div className={styles.dim_layer_container}>
+        <div className={styles.background_dim_layer}>
+          <label htmlFor='background_dim_layer' className={cx([hasDimLayer && styles.isChecked])}>
+            <BsLayersHalf />
+          </label>
+          <input
+            type='checkbox'
+            id='background_dim_layer'
+            checked={hasDimLayer}
+            onChange={handleCheckedDimLayerValue}
+          />
+        </div>
+
+        {style.blur && (
+          <div className={styles.opacity_container}>
+            <input type='range' value={style.blur} min='0' max='10' step={1} onChange={handleChangeOpacity} />
+          </div>
+        )}
       </div>
     </div>
   )
