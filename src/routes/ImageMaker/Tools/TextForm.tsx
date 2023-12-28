47e846455fae22a5077f395931e8445e0fae8597
이강윤 <65527334+rkddbs1031@users.noreply.github.com>
2023-12-28 11:51:48 +0900

    Fix: TextForm에 subTitle 추가

--- src/routes/ImageMaker/Tools/TextForm.tsx
+++ src/routes/ImageMaker/Tools/TextForm.tsx
@@ -3,6 +3,7 @@ import { useRecoilState } from 'recoil'
 
 import { styleState } from 'states/tool'
 
+import { cx } from 'styles'
 import styles from './toolContainer.module.scss'
 
 const TextForm = () => {
@@ -11,12 +12,33 @@ const TextForm = () => {
   const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) =>
     setStyle((prev) => ({ ...prev, title: e.currentTarget.value }))
 
+  const handleChangeSubTitle = (e: ChangeEvent<HTMLInputElement>) =>
+    setStyle((prev) => ({ ...prev, subTitle: e.currentTarget.value }))
+
   return (
     <div className={styles.text_container}>
       <dl>
-        <dd className={styles.text_option_title}>Text</dd>
+        <dd className={styles.text_option_title}>Title</dd>
         <dt className={styles.input_container}>
           <input id='title' type='text' value={style.title} className={styles.title} onChange={handleChangeTitle} />
+        </dt>
+      </dl>
+
+      <dl>
+        <dd className={cx(styles.text_option_title, styles.sub_title)}>
+          Subtitle
+          <div className={styles.sub_title_info}>
+            서브타이틀의 정렬, 색상, 두께 그리고 폰트 스타일은 Title과 동일하며, 폰트 사이즈만 비례하여 노출됩니다.
+          </div>
+        </dd>
+        <dt className={styles.input_container}>
+          <input
+            id='title'
+            type='text'
+            value={style.subTitle}
+            className={styles.subtitle}
+            onChange={handleChangeSubTitle}
+          />
         </dt>
       </dl>
     </div>
