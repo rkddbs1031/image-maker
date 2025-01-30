96bf3b17a5705e8959ffb4d6f36c0bff81b57715
Kangyoon Lee <rkddbs1031@gmail.com>
2025-01-30 16:53:10 +0900

    Fix: TextForm에 LabeledField 컴포넌트 적용

--- src/routes/ImageMaker/Tools/TextForm.tsx
+++ src/routes/ImageMaker/Tools/TextForm.tsx
@@ -1,11 +1,10 @@
-import { ChangeEvent } from 'react'
 import { useRecoilState } from 'recoil'
 
 import { styleState } from 'states/tool'
-
-import { cx } from 'styles'
-import styles from './toolContainer.module.scss'
 import { FormInput } from 'components/Input'
+import LabeledField from 'components/LabeledField'
+
+import styles from './toolContainer.module.scss'
 
 const TextForm = () => {
   const [style, setStyle] = useRecoilState(styleState)
@@ -16,29 +15,22 @@ const TextForm = () => {
 
   return (
     <div className={styles.text_container}>
-      <dl>
-        <dd className={styles.text_option_title}>Title</dd>
-        <dt className={styles.input_container}>
-          <FormInput.Text value={style.title} id='title' className={styles.title} onChange={handleChangeTitle} />
-        </dt>
-      </dl>
+      <LabeledField label='Title'>
+        <FormInput.Text value={style.title} id='title' className={styles.title} onChange={handleChangeTitle} />
+      </LabeledField>
 
-      <dl>
-        <dd className={cx(styles.text_option_title, styles.sub_title)}>
-          Subtitle
-          <div className={styles.sub_title_info}>
-            서브타이틀의 정렬, 색상, 두께 그리고 폰트 스타일은 Title과 동일하며, 폰트 사이즈만 비례하여 노출됩니다.
-          </div>
-        </dd>
-        <dt className={styles.input_container}>
-          <FormInput.Text
-            value={style.title}
-            id='sub_title'
-            className={styles.subTitle}
-            onChange={handleChangeSubTitle}
-          />
-        </dt>
-      </dl>
+      <LabeledField
+        label='Sub Title'
+        hasInfo
+        infoText='서브타이틀의 정렬, 색상, 두께 그리고 폰트 스타일은 Title과 동일하며, 폰트 사이즈만 비례하여 노출됩니다.'
+      >
+        <FormInput.Text
+          value={style.subTitle}
+          id='sub_title'
+          className={styles.subTitle}
+          onChange={handleChangeSubTitle}
+        />
+      </LabeledField>
     </div>
   )
 }
